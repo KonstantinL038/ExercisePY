7609a178ceb84ee987c5bafbc6b25bb2c0f16941
KonstantinL037 <k.nosov@g.nsu.ru>
2022-12-07 19:06:20 +0700

    ex 20

--- 20/20.py
+++ 20/20.py
@@ -3,16 +3,40 @@ import datetime
 
 
 class Load:
+    """load"""
+    meet_2 = []
+    person_meet = []
 
     @classmethod
     def write(cls, fl_meet, fl_pers, fl_pers_meet):
+
+        with open(fl_pers_meet, 'r', encoding='utf-8') as file_3:
+            first_line = file_3.readline()
+            for k in file_3:
+                person_meet = k.strip().split(';')[:-1]
+                if person_meet not in cls.person_meet:
+                    cls.person_meet.append(person_meet)
+
+        with open(fl_pers, 'r', encoding='utf-8') as file_2:
+            first_line = file_2.readline()
+            for k in cls.person_meet:
+                for j in file_2:
+                    person = User(*j.strip().split(';')[:-1])
+                    cls.meet_2.append(person)
+
         with open(fl_meet, 'r', encoding='utf-8') as file_1:
             first_line = file_1.readline()
             for i in file_1:
                 meet = Meeting(*i.strip().split(';')[:-1])
+                for k in cls.person_meet:
+                    for j in cls.meet_2:
+                        if int(k[0]) == int(meet.id):
+                            if int(k[1]) == int(j.id):
+                                meet.add_person(j)
 
 
 class Date:
+    """date"""
     months = ['янв', 'фев', 'мар', 'апр', 'май', 'июн', 'июл', 'авг', 'сен', 'окт', 'ноя', 'дек']
     day_months = [31, (28, 29), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
@@ -108,6 +132,7 @@ class Date:
         return '{}'.format(self.date)
 
 
+# Опишите здесь класс User.
 class User:
     """This class represent data about user.
     Has such positional attributes as: id, nick_name, first_name
@@ -142,12 +167,16 @@ class User:
         """Output depending on the number of attributes"""
         not_important = [self.last_name, self.middle_name, self.gender]
         output = 'ID: {} LOGIN: {} NAME: {}'.format(self.id, self.nick_name, self.first_name)
-        if not_important[0] is not None:
+        if not_important[0] != '':
             output = 'ID: {} LOGIN: {} NAME: {} {}'.format(self.id, self.nick_name, self.last_name, self.first_name)
-        if not_important[1] is not None:
+        if not_important[1] != '':
             output = output + ' {}'.format(self.middle_name)
-        if not_important[2] is not None:
+        if not_important[2] != '':
             output = output + ' GENDER: {}'.format(self.gender)
+
+        while '  ' in output:
+            output = output.replace('  ', ' ')
+
         return output
 
     def __repr__(self):
@@ -156,38 +185,60 @@ class User:
 
 
 class Meeting:
+    """meet"""
     lst_meeting = []
 
     def __init__(self, id, date, title):
         self.id = id  # ID
         self.date = Date(date)  # Дата
-        self.title = title      # Название мероприятия
+        self.title = title  # Название мероприятия
         self.employees = []  # Список сотрудников
         Meeting.lst_meeting.append(self)
 
     def add_person(self, person):
         """добавить сотрудника person"""
-        self.employees.append(person)
+        if person not in self.employees:
+            self.employees.append(person)
 
     @classmethod
     def total(cls):
         """Общее количество всех участников на всех конференциях."""
-        return len(cls.lst_meeting)
+        total = 0
+        for i in Meeting.lst_meeting:
+            total += len(i.employees)
+        return total
 
     @classmethod
     def count_meeting(cls, date):
         """Количество встреч на указанную дату date"""
         total = 0
-        for i in cls.lst_meeting:
-            if i.date == Date(date):
-                total += 1
+        if type(date) == Date:
+            for i in cls.lst_meeting:
+                if i.date == date:
+                    total += 1
+        else:
+            for i in cls.lst_meeting:
+                if i.date == Date(date):
+                    total += 1
         return total
 
+    def count(self):
+        """Определяющий количество сотрудников, зарегистрированных на встречу"""
+        return len(self.employees)
+
     def __str__(self):
-        return 'Рабочая встреча {}\n{} {}'.format(self.id, self.date, self.title)
+        answer = ''
+        for i in self.employees:
+            answer = answer + '{}\n'.format(i)
+
+        return 'Рабочая встреча {}\n{} {}\n{}'.format(self.id, self.date, self.title, answer)
 
 
-# Load.write('meetings.txt', 'persons.txt', 'pers_meetings.txt')
-# for item in Meeting.lst_meeting:
-#     print(item)
+Load.write('meetings.txt', 'persons.txt', 'pers_meetings.txt')
+for item in Meeting.lst_meeting:
+    print(item)
+
+print(Meeting.total())
+print(Meeting.count_meeting(Date('21.04.2020')))
+
 
